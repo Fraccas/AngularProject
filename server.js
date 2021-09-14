ee481f89f64bd159bba0b557bf70826edced481c
Fraccas <daltonward1@yahoo.com>
2021-09-13 21:34:50 -0600

    changes for heroku

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-/*const express = require('express');
-const app = express();
-const path = require('path');
+//Install express server    
+const express = require('express');
 
+const path = require('path');   
+
+const app = express();   
+
+// Serve only the static files form the dist directory    
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
 
-// PathLocationStrategy
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '../dist/index.html'));
-});
-
-console.log('Console listening...');*/
+console.log('Console listening...');
